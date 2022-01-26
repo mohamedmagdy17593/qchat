9a09a43599adf9cebd1aa02e47526a81a8839291
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2022-01-26 04:44:32 +0200

    design hoom page

--- src/components/HomePage/Connection.tsx
+++ src/components/HomePage/Connection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// const Connection = dynamic(() => import('./Connection'), { ssr: false });
 
 function Connection() {
   return (
---
+++ src/pages/new.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NewPage() {
+  return <div></div>;
+}
+
+export default NewPage;
