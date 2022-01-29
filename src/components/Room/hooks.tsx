be60ef46aa3883abbf62d0a8f4719493c1274c20
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2022-01-29 18:11:54 +0200

    try log

--- src/components/Room/hooks.tsx
+++ src/components/Room/hooks.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 
 export function useRoomId() {
   let router = useRouter();
+  console.log('router.query', router);
   let roomId = router.query.roomId as string;
   return roomId;
 }
