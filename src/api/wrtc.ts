9a25a2a67f6eec7b3a2390e5f7939a09dceaec9c
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2022-04-09 03:01:52 +0200

    clear config

--- src/api/wrtc.ts
+++ src/api/wrtc.ts
@@ -12,16 +12,6 @@ export const wrtc = {
     users.forEach((user) => {
       let peer = new SimplePeer({
         initiator: true,
-        config: {
-          iceServers: [
-            // ...stuns.map((s) => ({ urls: `stun:${s}` })),
-            {
-              urls: 'turn:numb.viagenie.ca',
-              username: 'webrtc@live.com',
-              credential: 'muazkh',
-            },
-          ],
-        },
       });
       setUpPeerEvents(peer, { userId: user.id });
     });
